a1cdccfbdb37c8d8cf6cf7a474c13b75d61395b8
sam <samuel.hofmann@klf.ch>
2022-07-09 14:55:25 +0200

    build.yaml adeed

--- main.go
+++ main.go
@@ -11,10 +11,18 @@ const KlfDateTimeFormat = "2006-01-02-15:04:05.999999999"
 
 func main() {
 	e := echo.New()
+
 	// ping
 	e.GET(UrlBasePath+"/ping", func(context echo.Context) error {
 		println(time.Now().Format(KlfDateTimeFormat) + " - Got request from " + context.RealIP())
 		return context.JSON(http.StatusOK, "pong")
+	})
+
+	// hello
+	e.GET(UrlBasePath+"/hello/:name", func(context echo.Context) error {
+		println(time.Now().Format(KlfDateTimeFormat) + " - Got request from " + context.RealIP())
+		name := context.Param("name")
+		return context.JSON(http.StatusOK, "Hello "+name)
 	})
 	e.Logger.Fatal(e.Start(":8200"))
 }
