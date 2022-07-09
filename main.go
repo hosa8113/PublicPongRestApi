e538cef2fb0eb5dd4b8b0e060e72b729154c85da
sam <samuel.hofmann@klf.ch>
2022-07-09 12:28:56 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"time"
+)
+
+const UrlBasePath = ""
+const KlfDateTimeFormat = "2006-01-02-15:04:05.999999999"
+
+func main() {
+	e := echo.New()
+	// ping
+	e.GET(UrlBasePath+"/ping", func(context echo.Context) error {
+		println(time.Now().Format(KlfDateTimeFormat) + " - Got request from " + context.RealIP())
+		return context.JSON(http.StatusOK, "pong")
+	})
+	e.Logger.Fatal(e.Start(":8200"))
+}
